9b2d67ac7ca364117441146eddf80575bc8503f0
yalaguresh <yalagureshgonal202@gamil.com>
2023-09-22 09:45:51 +0530

    python program

--- Part_B/roots.py
+++ Part_B/roots.py
@@ -1,12 +1,12 @@
 import math
 
 def find_roots(a, b, c):
-    discriminant = b**2 - 4*a*c
-    if discriminant > 0:
-        root1 = (-b + math.sqrt(discriminant)) / (2*a)
-        root2 = (-b - math.sqrt(discriminant)) / (2*a)
+    disc = b**2 - 4*a*c
+    if disc > 0:
+        root1 = (-b + math.sqrt(disc)) / (2*a)
+        root2 = (-b - math.sqrt(disc)) / (2*a)
         return root1, root2
-    elif discriminant == 0:
+    elif disc == 0:
         root = -b / (2*a)
         return root
     else:
